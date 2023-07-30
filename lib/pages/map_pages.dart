2df10a4ddbaf22f03b665c2b87a4bbb4b3ffaa09
tecnologyboy <lps76@hotmail.com>
2023-07-30 16:26:18 -0400

    meti cabra con el marcador del mapa

--- lib/pages/map_pages.dart
+++ lib/pages/map_pages.dart
@@ -19,12 +19,20 @@ class _MapPageState extends State<MapPage> {
     CameraPosition puntoInicial =
         CameraPosition(target: scan.getLatLng(), zoom: 17.5, tilt: 50);
 
+    Set<Marker> mapMarker = <Marker>{};
+
+    mapMarker.add(
+      Marker(
+          markerId: const MarkerId('Geo-Location'), position: scan.getLatLng()),
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Mapas'),
       ),
       body: GoogleMap(
         mapType: MapType.terrain,
+        markers: mapMarker,
         initialCameraPosition: puntoInicial,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
